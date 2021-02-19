e083c49131cb18f60f2c234e2636741ad11feb73
Coleman Buffa <coleman.buffa@gmail.com>
2021-02-18 22:56:38 -0800

    Commit to push to GitHub for BCS assistance.

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan");
-const Workout = require("./models/workout.js");
+// const logger = require("morgan");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(logger("dev"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // routes?
-// app.use(require("./routes/api.js"));
+// app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
