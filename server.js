b2878f24b178111d0cb81bccb6aa8d954a58f658
Coleman Buffa <coleman.buffa@gmail.com>
2021-02-19 15:35:20 -0800

    Add HTML routes and test on browser.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const logger = require("morgan");
+const logger = require("morgan");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// app.use(logger("dev"));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.use(express.static("public"));
+app.use(express.static("./public"));
 
-// routes?
+app.use(require("./routes/html-routes.js"));
 // app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
