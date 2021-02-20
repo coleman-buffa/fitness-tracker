89529ab0f9eb30fc4fb58d55e597e5ad41a41640
Coleman Buffa <coleman.buffa@gmail.com>
2021-02-19 21:12:13 -0800

    Add workout route that appends workout to a day's activities.

--- models/workout.js
+++ models/workout.js
@@ -27,22 +27,32 @@ const WorkoutSchema = new Schema({
       weight: {
         type: Number,
         min: 1
-      },  
+      },
       reps: {
         type: Number,
         min: 1
-      },  
+      },
       sets: {
         type: Number,
         min: 1
       },
       distance: {
         type: Number,
-        min:1
+        min: 1
       }
     }
-  ]      
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0 
+  }
 });
+
+WorkoutSchema.methods.getTotalDuration = function() {
+  exercises.forEach(element => {
+    this.totalDuration += element.duration;
+  });
+}
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
 router.get("/api/workouts", (req, res) => {
-  //Do work to return most recent workout
   Workout.find({})
     .then(data => {
       res.json(data);
@@ -14,11 +13,22 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-  //Do work to add exercise data
-});
+  console.log(req.body);
+  Workout.findOneAndUpdate({
+    _id: req.params.id},
+    {$push: { exercises: req.body}},
+    {new: true,
+    upsert: true})
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
 
 router.post("/api/workouts", (req, res) => {
-  //Do work to create a workout
+  // Create a new workout
 });
 
 router.get("/api/workouts/range", (req, res) => {
